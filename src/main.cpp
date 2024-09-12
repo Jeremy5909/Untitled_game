27567a12aa21e474ff63c590faff138936702e69
Jeremy5909 <jeremyherczeg@gmail.com>
2024-09-12 12:59:52 -0700

    Enter the third dimension

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,42 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
+#include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/matrix_projection.hpp"
+#include "glm/ext/matrix_transform.hpp"
+#include "glm/trigonometric.hpp"
 #include "shaderClass.h"
 #include "Texture.h"
 #include "VAO.h"
 #include "VBO.h"
 #include "EBO.h"
 
+const unsigned int width = 800;
+const unsigned int height = 800;
 
 // Vertices coordinates
 GLfloat vertices[] =
-{ //    COORDINATES       /      COLORS           TEXTURE  //
-    -0.5f, -0.5f, 0.05f,    1.0f, 0.0f, 0.0f,   0.0f, 0.0f,// Bottom left corner
-    -0.5f, 0.5f, 0.0f,      0.0f, 1.0f, 0.0f,   0.0f, 1.0f,// Top left corner
-    0.5f, 0.5f, 0.0f,       0.0f, 0.0f, 1.0f,   1.0f, 1.0f,// Top right corner
-    0.5f, -0.5f, 0.0f,      1.0f, 1.0f, 1.0f,   1.0f, 0.0f,// Bottom left corner
+{ //    COORDINATES       /      COLORS           TEXTURE 
+    -0.5f, 0.0f,  0.5f,     0.83f,  0.70f, 0.44f,   0.0f, 0.0f,
+    -0.5f, 0.0f, -0.5f,     0.83f,  0.70f, 0.44f,   5.0f, 0.0f,
+     0.5f, 0.0f, -0.5f,     0.83f,  0.70f, 0.44f,   0.0f, 0.0f,
+     0.5f, 0.0f,  0.5f,     0.83f,  0.70f, 0.44f,   5.0f, 0.0f,
+     0.0f, 0.8f,  0.0f,     0.92f,  0.86f, 0.76f,   2.5f, 5.0f,
 };
 
 // Indices for vertices order
 GLuint indices[] =
 {
-    0,2,1,
-    0,3,2,
+    0, 1, 2,
+    0, 2, 3,
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
+    3, 0, 4,
 };
 
 int main() {
@@ -32,7 +46,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Minecraft", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(width, height, "Minecraft", NULL, NULL);
 
     // Failed to create GLFW window
     if (window == NULL) {
@@ -44,7 +58,7 @@ int main() {
     glfwMakeContextCurrent(window);
 
     gladLoadGL();
-    glViewport(0, 0, 800, 800);
+    glViewport(0, 0, width, height);
 
     Shader shaderProgram("Shaders/default.vert.glsl", "Shaders/default.frag.glsl");
 
@@ -73,14 +87,43 @@ int main() {
     dirt.texUnit(shaderProgram, "tex0", 0);
 
     glClearColor(0.2f,0.3f,0.3f,1.0f);
+
+    float rotation = 0.0f;
+    double prevTime = glfwGetTime();
+
+    glEnable(GL_DEPTH_TEST);
+
     while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         shaderProgram.Activate();
+
+        double currentTime = glfwGetTime();
+
+        if (currentTime - prevTime >= 1/60) {
+            rotation += 0.5f;
+            prevTime = currentTime;
+        }
+
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 proj = glm::mat4(1.0f);
+
+        model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+        proj = glm::perspective(glm::radians(45.0f), (float)(width/height), 0.1f, 100.0f);
+
+        int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
         glUniform1f(uniID, 0.5f);
         dirt.Bind();
         VAO1.Bind();
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
 
         // Check for events
